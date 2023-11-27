9a92156093fd2aeb51873eeb0ebac88aacf909f5
aalis <andreylisogor@gmail.com>
2023-11-27 17:37:53 +0200

    2nd commit

--- characters.py
+++ characters.py
@@ -16,6 +16,9 @@ class BaseCharacter:
     def defence(self, target):
         pass
 
+    def display_stats(self):
+        print(f"Name: {self.name}, Strength: {self.strength}, Health: {self.health}")
+
 
 class Equipment:
     def __init__(self):
@@ -49,23 +52,97 @@ class Inventory:
 
 class TheElementalMage(BaseCharacter):
     def __init__(
-        self, name, strength, agility, intelligence, experience, level, health=100
+        self, name, strength, agility, intelligence, experience, level, health
     ):
         super().__init__(
             name, strength, agility, intelligence, experience, level, health
         )
-        self.name = name
-        self.strength = strength
-        self.agility = agility
-        self.intelligence = intelligence
-        self.experience = experience
-        self.level = level
-        self.health = health
-        self._inventory = Inventory()
-        self, _equipment = Equipment()
 
     def attack(self, target):
+        # TODO:special attack
         pass
 
     def defence(self, target):
+        # TODO:special defence
         pass
+
+
+class TheStealthyRogue(BaseCharacter):
+    def __init__(
+        self, name, strength, agility, intelligence, experience, level, health
+    ):
+        super().__init__(
+            name, strength, agility, intelligence, experience, level, health
+        )
+
+    def attack(self, target):
+        # TODO:special attack
+        pass
+
+    def defence(self, target):
+        # TODO:special defence
+        pass
+
+
+class TheResillientWarrior(BaseCharacter):
+    def __init__(
+        self, name, strength, agility, intelligence, experience, level, health
+    ):
+        super().__init__(
+            name, strength, agility, intelligence, experience, level, health
+        )
+
+    def attack(self, target):
+        # TODO:special attack
+        pass
+
+    def defence(self, target):
+        # TODO:special defence
+        pass
+
+
+class TheSkilledArcher(BaseCharacter):
+    def __init__(
+        self, name, strength, agility, intelligence, experience, level, health
+    ):
+        super().__init__(
+            name, strength, agility, intelligence, experience, level, health
+        )
+
+    def attack(self, target):
+        # TODO:special attack
+        pass
+
+    def defence(self, target):
+        # TODO:special defence
+        pass
+
+
+class TheMysticalDruid(BaseCharacter):
+    def __init__(
+        self, name, strength, agility, intelligence, experience, level, health
+    ):
+        super().__init__(
+            name, strength, agility, intelligence, experience, level, health
+        )
+
+    def attack(self, target):
+        # TODO:special attack
+        pass
+
+    def defence(self, target):
+        # TODO:special defence
+        pass
+
+
+if __name__ == "__main__":
+    p = TheMysticalDruid(
+        name="Dron",
+        strength=89,
+        agility=9,
+        experience=8,
+        level=0,
+        intelligence=98,
+        health=100,
+    )
+    p.display_stats()
